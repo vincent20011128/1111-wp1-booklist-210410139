ea3df246b350649c91b313259b8cf8c52013b08b
vincent20011128 <210410139@gms.tku.edu.tw>
2022-09-14 22:03:13 +0800

    ### W02-P5: copy W02 files to demo/w02_booklist

---
+++ demo/w02_booklist/src/App_39.js
@@ -0,0 +1,10 @@
+import Book_39 from './components/Book_39';
+import './App_39.css';
+
+import Booklist_39 from './components/Booklist_39';
+
+const App_39 = () => {
+  return <Booklist_39 />
+};
+
+export default App_39;
