6979c49e1d99f7e31fec0556b54d588620554c5b
vincent20011128 <210410139@gms.tku.edu.tw>
2022-09-28 20:44:41 +0800

    W04-P3: Hooks Tutorial T13_xx, useState using array of objects with remove item capability

--- src/App_xx.js
+++ src/App_xx.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T12_39 from './tutorial/1-useState/T12_39-useState-basics.js';
+import T13_39 from './tutorial/1-useState/T13_39_useState-array.js';
 
 const App_xx = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 12 - 210410139</h2>
-      <T12_39 />
+      <h2>Tutorial 13 - 210410139</h2>
+      <T13_39 />
     </div>
   );
 };
