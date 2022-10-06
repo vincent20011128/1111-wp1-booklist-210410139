0762dd9a1756dec5dc756b81de64d2e7094eee5b
vincent20011128 <210410139@gms.tku.edu.tw>
2022-10-06 21:49:36 +0800

    W05-P4: T31_xx: conditional rendering -- 3 cases

--- src/App_xx.js
+++ src/App_xx.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T23_39 from './tutorial/2-useEffect/T23_39-useEffect-fetch-data.js';
+import T31_39 from './tutorial/3-conditional-rendering/T31_39-multiple-returns.js';
 
 const App_xx = () => {
   return (
     <div className='container'>
-      <h2>Tutorial T23_39</h2>
-      <T23_39 />
+      <h2>Tutorial T31_39</h2>
+      <T31_39 />
     </div>
   );
 };
