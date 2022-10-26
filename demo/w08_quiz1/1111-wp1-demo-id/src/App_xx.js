e1d962305da682ac811c16a8818468fe4b9d0cc6
vincent20011128 <210410139@o365.tku.edu.tw>
2022-10-26 22:09:07 +0800

    W08-P7: 實作 Alert_xx.js，能夠顯示第 4 題 Delete 及第 5 題 Clear All Blogs 的訊息，顏色都是 danger 的紅色。

--- demo/w08_quiz1/1111-wp1-demo-id/src/App_xx.js
+++ demo/w08_quiz1/1111-wp1-demo-id/src/App_xx.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import data from './components/BlogData_xx';
 import BlogList_xx from './components/BlogList_xx';
-// import Alert_xx from './components/Alert_xx';
+import Alert_39 from './components/Alert_39';
 
 const App_39 = () => {
   const [blogs, setBlogs] =  useState(data);
   console.log('blogs',blogs);
+  const [alert,setAlert] = useState({
+    show: false,
+    msg:'',
+    type: ''
+  });
 
   const removeItem = (id) => {
     setBlogs(blogs.filter((blog) => blog.id !== id));
+    showAlert(true,'item removed','danger');
   }
 
   const clearBlogs = () => {
     setBlogs([]);
+    showAlert(true,'empty list','danger');
   }
 
   const filterItem = (category) => {
@@ -23,10 +30,15 @@ const App_39 = () => {
       setBlogs(newBlogs);
     }
   }
+  const showAlert = (show = false, msg = '', type = '') => {
+    setAlert({show, msg, type });
+  };
+  
 
   return (
     <>
     <section className="blogs">
+    {alert.show&& <Alert_39 {...alert} removeAlert={showAlert}/> }
       <div className="section-title">
         <h2>CSS Grid using breakpoints</h2>
       </div>
