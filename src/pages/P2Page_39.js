a7c2df5a191ec69ef2b051f20702761f5f7517b6
vincent20011128 <210410139@gms.tku.edu.tw>
2022-11-16 20:42:52 +0800

    W11-P2_xx: for midterm P3b

--- src/pages/P2Page_39.js
+++ src/pages/P2Page_39.js
@@ -23,20 +23,22 @@ const P2Page_39 = () => {
     </div>
     <div className="products-right">
       <div className="products-container">
-        <div className="single-product">
-          <img src="./images/product-3.jpg" className="single-product-img img" alt="high-back bench" />
-          <footer>
-            <h5 className="name">high-back bench</h5>
-            <span className="price">$9.99</span>
-          </footer>
-        </div>
-        <div className="single-product">
-          <img src="./images/product-9.jpg" className="single-product-img img" alt="albany table" />
-          <footer>
-            <h5 className="name">albany table</h5>
-            <span className="price">$79.99</span>
-          </footer>
-        </div>
+      
+        { products.map((product)=> {
+          const{ img, name, price } = product;
+          return (
+            <div className="single-product">
+            <img 
+            src={img} 
+            className="single-product-img img" 
+            alt="albany table" />
+            <footer>
+              <h5 className="name">{name}</h5>
+              <span className="price">{price}</span>
+            </footer>
+          </div>
+          )
+        }) }
       </div>
     </div>
   </section>
